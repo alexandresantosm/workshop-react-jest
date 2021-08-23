b26293930ddbb8a61c1e1cba9d03db51a981bcd5
Alexandre Menezes <amenezes031@gmail.com>
2021-08-23 15:18:13 -0300

    test if image was rendered

--- src/components/EmptyResult/__test__/EmptyResult.spec.js
+++ src/components/EmptyResult/__test__/EmptyResult.spec.js
@@ -8,8 +8,17 @@ describe("EmptyResult", () => {
   test("should be render with default props", () => {
     // Teste propriamente dito
     const { container, getByAltText } = render(<EmptyResult />);
+    /*
+      Captura de elementos
+      image: atributo alt
+      input: atributo placehold
+      button: conteudo (texto) do botao
+      alguns casos: atributo title
+    */
+    const image = getByAltText("Empty Result");
 
-    expect(getByAltText("Empty Result")).toBeInTheDocument(); // Valida se foi renderizado um elemento com o texto alternativo
     expect(container).toBeInTheDocument(); // Valida se o componente foi renderizado na tela
+    expect(image).toBeInTheDocument(); // Valida se a imagem foi renderizada
+    expect(image.width).toBe(200); // Valida se a imagem foi renderizada com o tamanho padrao
   });
 });
