af0da02968ce119771f4a547a1a40bd261031447
Alexandre Menezes <amenezes031@gmail.com>
2021-08-23 18:50:48 -0300

    test if onClick event was called

--- src/components/PokemonCard/__test__/PokemonCard.spec.js
+++ src/components/PokemonCard/__test__/PokemonCard.spec.js
@@ -4,7 +4,7 @@ import { render } from "@testing-library/react";
 import { cardBuilder } from "../../../__mocks__/CardBuilder";
 import PokemonCard from "../PokemonCard";
 
-const setup = (card, onClick) => {
+const setup = ({ card, onClick }) => {
   const defaultCard = card || cardBuilder();
   const renderResult = render(
     <PokemonCard {...defaultCard} onClick={onClick} />
@@ -19,9 +19,18 @@ const setup = (card, onClick) => {
 
 describe("PokemonCard", () => {
   it("should be render with default props", () => {
-    const { container, image } = setup();
+    const { container, image } = setup({});
 
     expect(container).toBeInTheDocument();
     expect(image).toBeInTheDocument();
+  });
+
+  it("should be emit onClick event", () => {
+    const onClick = jest.fn();
+    const { image } = setup({ onClick });
+
+    image.click();
+
+    expect(onClick).toHaveBeenCalledTimes(1);
   });
 });
