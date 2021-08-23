75be9e374909f20e7c5131ba258794b8a743e470
Alexandre Menezes <amenezes031@gmail.com>
2021-08-23 18:29:11 -0300

    test if image was rendered

--- src/components/PokemonCard/__test__/PokemonCard.spec.js
+++ src/components/PokemonCard/__test__/PokemonCard.spec.js
@@ -1,20 +1,27 @@
 import React from "react";
 import { render } from "@testing-library/react";
 
+import { cardBuilder } from "../../../__mocks__/CardBuilder";
 import PokemonCard from "../PokemonCard";
 
-const setup = (props = {}) => {
-  const renderResult = render(<PokemonCard {...props} />);
+const setup = (card, onClick) => {
+  const defaultCard = card || cardBuilder();
+  const renderResult = render(
+    <PokemonCard {...defaultCard} onClick={onClick} />
+  );
 
   return {
     ...renderResult,
+    defaultCard,
+    image: renderResult.getByAltText(`${defaultCard.id}-${defaultCard.name}`),
   };
 };
 
 describe("PokemonCard", () => {
   it("should be render with default props", () => {
-    const { container } = setup();
+    const { container, image } = setup();
 
     expect(container).toBeInTheDocument();
+    expect(image).toBeInTheDocument();
   });
 });
