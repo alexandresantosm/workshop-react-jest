e16b8e52d6e2bb92d5d303042b569fe2c4cdfc6d
Alexandre Menezes <amenezes031@gmail.com>
2021-08-23 13:29:56 -0300

    test if has element with alternative text

--- src/components/EmptyResult/__test__/EmptyResult.spec.js
+++ src/components/EmptyResult/__test__/EmptyResult.spec.js
@@ -7,8 +7,9 @@ describe("EmptyResult", () => {
   // Faz um agrupamento de testes
   test("should be render with default props", () => {
     // Teste propriamente dito
-    const { container } = render(<EmptyResult />);
+    const { container, getByAltText } = render(<EmptyResult />);
 
+    expect(getByAltText("Empty Result")).toBeInTheDocument(); // Valida se foi renderizado um elemento com o texto alternativo
     expect(container).toBeInTheDocument(); // Valida se o componente foi renderizado na tela
   });
 });
