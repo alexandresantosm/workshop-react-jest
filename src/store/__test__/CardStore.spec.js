3ec432f385e16f2848dee0a0456a4c10e04fc34a
Alexandre Menezes <amenezes031@gmail.com>
2021-08-24 20:22:09 -0300

    test getCards

--- src/store/__test__/CardStore.spec.js
+++ src/store/__test__/CardStore.spec.js
@@ -1,8 +1,11 @@
 import { applyMiddleware, combineReducers, createStore } from "redux";
 import thunk from "redux-thunk";
-import { cardStateBuilder } from "../../__mocks__/CardStateBuilder";
 
+import { axiosMock } from "../../__mocks__/AxiosMock";
 import cardStore from "../card.store";
+import { cardStateBuilder } from "../../__mocks__/CardStateBuilder";
+import { cardBuilder } from "../../__mocks__/CardBuilder";
+import { arrayToObject, getArrayIds } from "../../__mocks__/utils";
 
 describe("CardStore", () => {
   let store;
@@ -22,5 +25,31 @@ describe("CardStore", () => {
 
   it("should be have correct initial state", () => {
     expect(store.getState()).toEqual({ card: initialCardsState });
+  });
+
+  it("should be dispatch getCards", async () => {
+    const card = cardBuilder();
+
+    axiosMock.get.mockResolvedValue({
+      data: {
+        cards: [card],
+      },
+    });
+
+    await store.dispatch(cardStore.actions.getCards({ query: "" }));
+
+    const currentState = store.getState();
+
+    expect(axiosMock.get).toHaveBeenCalledTimes(1);
+    expect(axiosMock.get).toHaveBeenCalledWith(
+      "/cards?page=1&name=&pageSize=27"
+    );
+
+    expect(currentState.card).toEqual({
+      ...initialCardsState,
+      cards: arrayToObject([card]),
+      ids: getArrayIds([card]),
+      query: "",
+    });
   });
 });
